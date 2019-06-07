b805e5d2820af56cafd3a69416eed89088dbf86b
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2019-06-07 13:55:11 -0300

    Add new test to check context using envvar

--- tests/test_envvars.py
+++ tests/test_envvars.py
@@ -31,3 +31,19 @@ def test_simple_get_request(django_testdir):
     result = django_testdir.runpytest('--envvars-validate')
     assert result.ret == 0
     result.stdout.fnmatch_lines(["*test_get_home_page PASSED*"])
+
+
+def test_read_envvar_from_context_without_set_environment(django_testdir):
+    django_testdir.create_test_module("""
+        import pytest
+
+        pytestmark = pytest.mark.django_db
+
+        def test_context_values(client):
+            response = client.get('/')
+            assert response.context['pytest_envvar_str'] == 'Rafael'
+    """)
+
+    result = django_testdir.runpytest('--envvars-validate')
+    assert result.ret == 0
+    result.stdout.fnmatch_lines(["*test_context_values PASSED*"])
