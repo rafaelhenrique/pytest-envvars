e493e23f8596cab3d26dade0d28f1faea1033b18
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2019-06-06 22:22:35 -0300

    Make first tests to test tests of test project :P

--- tests/test_envvars.py
+++ tests/test_envvars.py
@@ -1,10 +1,33 @@
+def test_simple_model_test(django_testdir):
+    django_testdir.create_test_module("""
+        import pytest
 
-def test_dummy(testdir):
-    testdir.makepyfile("""
-            def test_dummy():
-                assert 1 == 1
-        """)
+        from tests.pytest_envvars_django_test.core.models import Product
 
-    result = testdir.runpytest('--envvars-validate')
+        pytestmark = pytest.mark.django_db
 
+        def test_product_exists_with_products():
+            Product.objects.create(name='Mouse')
+            assert Product.objects.exists() is True
+    """)
+
+    result = django_testdir.runpytest('--envvars-validate')
     assert result.ret == 0
+    result.stdout.fnmatch_lines(["*test_product_exists_with_products PASSED*"])
+
+
+def test_simple_get_request(django_testdir):
+    django_testdir.create_test_module("""
+        import pytest
+
+        pytestmark = pytest.mark.django_db
+
+        def test_get_home_page(client):
+            response = client.get('/')
+            assert response.status_code == 200
+            assert 'Only for test purpouses' in str(response.content)
+    """)
+
+    result = django_testdir.runpytest('--envvars-validate')
+    assert result.ret == 0
+    result.stdout.fnmatch_lines(["*test_get_home_page PASSED*"])
