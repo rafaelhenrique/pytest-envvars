b2758def0a2dbad3e18926d92ca8c982b4fb17de
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2019-06-14 12:30:40 -0300

    Remove unused lines

--- pytest_envvars/validators.py
+++ pytest_envvars/validators.py
@@ -1,16 +1,8 @@
 import random
 import os
 
-from importlib import reload
-
 
 class PytestEnvvarsValidator:
 
     def pytest_runtest_setup(self, item):
         os.environ['PYTEST_ENVVAR_STR'] = random.choice(['0', '1'])
-
-        from django import conf
-        from pytest_envvars_django_test import settings
-
-        reload(conf)
-        reload(settings)
