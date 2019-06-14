fbe40c53144074b2db05cb5570cf2b33592a7784
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2019-06-14 11:27:00 -0300

    Improve structure of tests
    Split into 2 modules:
    
    - First module (test_django_application.py): Tests about django application;
    - Second module (test_envvars.py): Tests about pytest-envvars.

---
+++ tests/test_django_application.py
@@ -0,0 +1,33 @@
+def test_app_model_test(django_testdir):
+    django_testdir.create_test_module("""
+        import pytest
+
+        from tests.pytest_envvars_django_test.core.models import Product
+
+        pytestmark = pytest.mark.django_db
+
+        def test_product_exists_with_products():
+            Product.objects.create(name='Mouse')
+            assert Product.objects.exists() is True
+    """)
+
+    result = django_testdir.runpytest('--envvars-validate')
+    assert result.ret == 0
+    result.stdout.fnmatch_lines(["*test_product_exists_with_products PASSED*"])
+
+
+def test_app_get_request(django_testdir):
+    django_testdir.create_test_module("""
+        import pytest
+
+        pytestmark = pytest.mark.django_db
+
+        def test_get_home_page(client):
+            response = client.get('/')
+            assert response.status_code == 200
+            assert 'Only for test purpouses' in str(response.content)
+    """)
+
+    result = django_testdir.runpytest('--envvars-validate')
+    assert result.ret == 0
+    result.stdout.fnmatch_lines(["*test_get_home_page PASSED*"])
